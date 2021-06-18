d8d6ed42e70e7659238a10d9f3ee2268f7bfe6e3
Richard Ross <richardjrossiii@gmail.com>
2021-06-18 14:39:05 -0700

    Auto table widths

--- conventor.py
+++ conventor.py
@@ -1,4 +1,4 @@
-import re 
+import re
 import yaml
 from yamlinclude import YamlIncludeConstructor
 import prettytable
@@ -12,7 +12,6 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 
-
 class Conventor:
     macros: Dict[str, str]
     sections: List[Dict[str, str]]
@@ -22,51 +21,51 @@ class Conventor:
     def __init__(self, input_path: Path):
         input_data = yaml.safe_load(open(input_path))
 
-        include = input_data.pop('include')
+        include = input_data.pop("include")
         for i in include:
             i_path = (input_path.parent / i).resolve()
             if i_path.suffix != ".yaml":
                 logger.warning(
-                    f"Cowardly refusing to include content for file {i} " +
-                    "it doesn't appear to be a YAML file!"
+                    f"Cowardly refusing to include content for file {i} "
+                    + "it doesn't appear to be a YAML file!"
                 )
-            
+
             if input_path.parent.resolve() not in i_path.parents:
                 logger.warning(
-                    f"Cowardly refusing to include content for file {i} " +
-                    "it appears to escape the parent directory!"
+                    f"Cowardly refusing to include content for file {i} "
+                    + "it appears to escape the parent directory!"
                 )
                 continue
 
             included_data = yaml.safe_load(open(i_path))
 
             input_data.update(
-                { 
-                    (i_path.stem + "/" + key): value 
-                    for key, value in included_data.items() 
+                {
+                    (i_path.stem + "/" + key): value
+                    for key, value in included_data.items()
                 }
             )
 
-        self.macros = input_data.pop('macros')
-        self.sections = input_data.pop('sections')
+        self.macros = input_data.pop("macros")
+        self.sections = input_data.pop("sections")
         self.tables = input_data
 
     def process(self, output_dir: Path):
         for table, contents in self.tables.items():
             if table == "index":
                 logger.warning(
-                    "Skipping table named index, as it " + 
-                    "would be overwritten anyway!"
+                    "Skipping table named index, as it "
+                    + "would be overwritten anyway!"
                 )
                 continue
 
-            rst_table_path = (output_dir / f"{table}.rst")
+            rst_table_path = output_dir / f"{table}.rst"
 
             # Make sure output path is safe
             if output_dir.resolve() not in rst_table_path.resolve().parents:
                 logger.warning(
-                    f"Cowardly refusing to write content for table {table} " +
-                    "it appears to escape the parent directory!"
+                    f"Cowardly refusing to write content for table {table} "
+                    + "it appears to escape the parent directory!"
                 )
 
             rst_table_path.parent.mkdir(parents=True, exist_ok=True)
@@ -78,7 +77,7 @@ class Conventor:
         publish_file(
             source_path=str(output_dir / "index.rst"),
             destination_path=str(output_dir / "index.html"),
-            writer_name='html',
+            writer_name="html",
         )
 
     def get_rst_table(self, name: str, contents: Optional[Dict[str, str]]) -> str:
@@ -93,39 +92,46 @@ class Conventor:
         table.align["b"] = "l"
 
         for left, right in contents.items():
-            table.add_row([
-                self.macro_substitute(left, 'left'),
-                self.macro_substitute(right, 'right'),
-            ])
+            table.add_row(
+                [
+                    self.macro_substitute(left, "left"),
+                    self.macro_substitute(right, "right"),
+                ]
+            )
 
-        return table.get_string()
+        table_contents = f".. table::\n    :widths: auto\n\n"
+
+        for row in table.get_string().split("\n"):
+            table_contents += f"    {row}\n"
+
+        return table_contents
 
     def get_index_rst(self) -> str:
         output = ""
 
         for section in self.sections:
-            title = section.get('title')
-            anchor = section.get('anchor')
-            contents = section.get('contents')
-            hidden = section.get('hidden', False)
+            title = section.get("title")
+            anchor = section.get("anchor")
+            contents = section.get("contents")
+            hidden = section.get("hidden", False)
 
             if title is None or contents is None:
                 logger.warning("Empty section found!")
                 continue
 
-            title = self.macro_substitute(title, 'title')
-            contents = self.macro_substitute(contents, 'contents')
+            title = self.macro_substitute(title, "title")
+            contents = self.macro_substitute(contents, "contents")
 
             max_line_length = max(map(len, title.split("\n")))
 
-            if not hidden: 
+            if not hidden:
                 output += "\n"
 
                 if anchor:
                     output += f".. _{anchor}:"
                     output += "\n\n"
 
-                output += "-" * max_line_length 
+                output += "-" * max_line_length
                 output += "\n"
                 output += title
                 output += "\n"
@@ -138,8 +144,8 @@ class Conventor:
 
     def macro_substitute(self, content: str, section: str) -> str:
         macros_to_process = {
-            **self.macros['everywhere'],
-            **self.macros.get(section, {})
+            **self.macros["everywhere"],
+            **self.macros.get(section, {}),
         }
         # TODO: Compile these ahead of time for performance
         for find, replace in macros_to_process.items():
--- main.py
+++ main.py
@@ -9,19 +9,10 @@ from conventor import Conventor
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        'input_path', 
-        type=str, 
-        help='YAML path to process'
-    )
-    parser.add_argument(
-        'output_dir', 
-        type=str, 
-        help='Path to dump output RST files'
-    )
+    parser.add_argument("input_path", type=str, help="YAML path to process")
+    parser.add_argument("output_dir", type=str, help="Path to dump output RST files")
 
     arguments = parser.parse_args()
 
     conventor = Conventor(Path(arguments.input_path))
     conventor.process(Path(arguments.output_dir))
-
